87fd2846e139dd02adcc5eb3a2c380a3538f2db1
snorock <ibrovche@gmail.com>
2018-03-14 20:46:40 -0400

    added parameters to gameobject class

--- Scripts/objects/gameobject.ts
+++ Scripts/objects/gameobject.ts
@@ -5,11 +5,14 @@ module objects {
     protected _dy: number;
 
     // public properties
+    public tag: String;
     public width: number;
     public height: number;
     public halfWidth: number;
     public halfHeight: number;
     public isColliding: boolean;
+    public A: math.Vec2; //left apper corner
+    public B: math.Vec2; //right bottom corner
 
     // constructors
     constructor(assetManager: createjs.LoadQueue, imageString:string) {
@@ -18,13 +21,20 @@ module objects {
       this._initialize();
   }
     // private methods
-    private _initialize():void {
+    private _initialize(t ?: String):void {
+      if(t){
+        this.tag = t;
+      } else {
+        this.tag = "";
+      }
       this.width = this.getBounds().width;
       this.height = this.getBounds().height;
       this.halfWidth = this.width * 0.5;
       this.halfHeight = this.height * 0.5;
       this.regX = this.halfWidth;
       this.regY = this.halfHeight;
+      this.A = new math.Vec2(this.x-this.halfWidth, this.y-this.halfHeight); 
+      this.B = new math.Vec2(this.x+this.halfWidth, this.y+this.halfHeight);
       this.isColliding = false;
     }
 
