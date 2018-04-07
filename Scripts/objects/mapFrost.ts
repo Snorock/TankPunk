48abb06c18a9e150c71bdc9a1a458a39413b4e34
Bruno Campos <hello.bruno.campos@gmail.com>
2018-04-07 14:17:40 -0400

    Insert new elements for the level 3

--- Scripts/objects/mapFrost.ts
+++ Scripts/objects/mapFrost.ts
@@ -7,7 +7,7 @@ module objects {
   
       // Constructor
       constructor(assetManager: createjs.LoadQueue) {
-        super(assetManager.getResult("frost"));
+        super(assetManager.getResult("frostBackground"));
         this.Start();
       }
   
